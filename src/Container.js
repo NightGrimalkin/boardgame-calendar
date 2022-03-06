f48256c36467e53838929f34e3286b625a30b625
NightGrimalkin <mateusz.mybox@gmail.com>
2022-03-06 23:35:43 +0100

    określenie gdzie powinien miescic sie stan oraz dodanie przepływu danych

--- src/Container.js
+++ src/Container.js
@@ -3,17 +3,38 @@ import Game from "./Game";
 import "./container.css";
 
 function Container(props) {
+  const [gameInfo, setGameInfo] = useState(0);
+
   useEffect(() => {
-    const gameInfo = props.gameInfo;
-    console.log(gameInfo);
-  });
+    const constGameInfo = props.gameInfo;
+    setGameInfo(constGameInfo);
+  }, []);
+
+  function returnGames() {
+    const tempGameInfo = gameInfo;
+    return tempGameInfo.map((value, index) => {
+      return (
+        <Game
+          key={index + " " + value}
+          name={value.name}
+          premiere={value.premiere}
+        />
+      );
+    });
+  }
 
   return (
     <>
+      
+      <div className="containerElemArea">
       <h2>{props.month}</h2>
-      <div className="containerElem">
-        <Game name="darkest" premiere="yes" />
-        <Game name="darkest" premiere="yes" />
+        <div className="containerElem">
+          {gameInfo == 0 ? (
+            <h3>W tym miesiącu nie wychodzi żadna gra</h3>
+          ) : (
+            returnGames()
+          )}
+        </div>
       </div>
     </>
   );
--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Container from "./Container";
+import "./main.css";
 
 function Main() {
+  const myRefMonths = useRef([]);
+  const myRefYears = useRef([]);
+  const [sortBy, setSortBy] = useState(1);
+  const [years, setYears] = useState([]);
   const months = [
     "January",
     "Febuary",
@@ -22,49 +27,135 @@ function Main() {
       premiere: "yes",
       day: 20,
       month: "May",
-      year: "2020",
+      year: 2020,
+    },
+    {
+      name: "Darkest Dungeon",
+      premiere: "yes",
+      day: 20,
+      month: "May",
+      year: 2021,
     },
     {
       name: "Darkest Dungeon",
       premiere: "yes",
       day: 1,
       month: "May",
-      year: "2020",
+      year: 2020,
     },
     {
       name: "Darkest Dungeon",
       premiere: "yes",
       day: 2,
       month: "May",
-      year: "2020",
+      year: 2020,
     },
     {
       name: "Darkest Dungeon",
       premiere: "no",
       day: 3,
       month: "May",
-      year: "2020",
+      year: 2020,
     },
     {
       name: "Darkest Dungeon",
       premiere: "no",
       day: 4,
       month: "May",
-      year: "2020",
+      year: 2020,
     },
     {
       name: "Darkest Dungeon",
       premiere: "no",
       day: 5,
       month: "May",
-      year: "2020",
+      year: 2020,
+    },
+    {
+      name: "Darkest Dungeon",
+      premiere: "no",
+      day: 5,
+      month: "March",
+      year: 2020,
+    },
+    {
+      name: "Darkest Dungeon",
+      premiere: "no",
+      day: 5,
+      month: "March",
+      year: 2027,
+    },
+    {
+      name: "Darkest Dungeon",
+      premiere: "no",
+      day: 5,
+      month: "March",
+      year: 2026,
+    },
+    {
+      name: "Darkest Dungeon",
+      premiere: "no",
+      day: 5,
+      month: "March",
+      year: 2025,
+    },
+    {
+      name: "Darkest Dungeon",
+      premiere: "no",
+      day: 5,
+      month: "March",
+      year: 2024,
+    },
+    {
+      name: "Darkest Dungeon",
+      premiere: "no",
+      day: 5,
+      month: "March",
+      year: 2023,
+    },
+    {
+      name: "Darkest Dungeon",
+      premiere: "no",
+      day: 5,
+      month: "March",
+      year: 2022,
     },
   ];
+
+  function executeScrollMonths(index) {
+    console.log(myRefMonths.current.length);
+    myRefMonths.current[index].scrollIntoView();
+  }
+  function executeScrollYears(index) {
+    console.log(myRefYears.current.length);
+    myRefYears.current[index].scrollIntoView();
+  }
+
+  function getAllYears() {
+    let years = [];
+    info.forEach((element) => {
+      if (!years.includes(element.year)) {
+        years.push(element.year);
+      }
+    });
+    years.sort((a, b) => a - b);
+    return years;
+  }
+
+  function getGamesFromYear(year) {
+    let gamesFromYear = [];
+    info.forEach((element) => {
+      if (element.year == year) {
+        gamesFromYear.push(element);
+      }
+    });
+    return gamesFromYear;
+  }
 
   function getGamesFromMonth(month) {
     let gamesFromMonth = [];
     info.forEach((element) => {
-      if (element.month == month) {
+      if (element.month == month && element.year == new Date().getFullYear()) {
         gamesFromMonth.push(element);
       }
     });
@@ -72,41 +163,104 @@ function Main() {
   }
 
   function returnContainers() {
+    myRefMonths.current = [];
+    myRefYears.current = [];
     let containers = months;
-    let gamesFromMonth;
-    return containers.map((element, index) => {
-      return (
-        <Container
-          key={index + " " + element}
-          gameInfo={getGamesFromMonth(element)}
-          month={element}
-        />
-      );
-    });
+    if (sortBy == 0) {
+      return containers.map((element, index) => {
+        return (
+          <section
+            ref={(el) => myRefMonths.current.push(el)}
+            key={index + " " + element}
+          >
+            <Container gameInfo={getGamesFromMonth(element)} month={element} />
+          </section>
+        );
+      });
+    }
+    if (sortBy == 1) {
+      return years.map((element, index) => {
+        return (
+          <section
+            ref={(el) => myRefYears.current.push(el)}
+            key={index + " " + element}
+          >
+            <Container gameInfo={getGamesFromYear(element)} month={element} />
+          </section>
+        );
+      });
+    }
   }
   const returnMonths = () => {
-    return months.map((value, index) => {
-      return <input key={index + " " + value} type="button" value={value} />;
-    });
+    if (sortBy == 0) {
+      return months.map((value, index) => {
+        return (
+          <button
+            key={index + " " + value}
+            onClick={() => executeScrollMonths(index)}
+            className="link-buttons"
+          >
+            {value}
+          </button>
+        );
+      });
+    }
+    if (sortBy == 1) {
+      return years.map((value, index) => {
+        return (
+          <button
+            key={index + " " + value}
+            onClick={() => executeScrollYears(index)}
+            className="link-buttons"
+          >
+            {value}
+          </button>
+        );
+      });
+    }
   };
 
+  function changeSort(e) {
+    if (e.target.value == "years") {
+      setSortBy(1);
+    }
+    if (e.target.value == "months") {
+      setSortBy(0);
+    }
+  }
+
   useEffect(() => {
-    //console.table(getGamesFromMonth("May"));
-  });
+    //console.log(getAllYears());
+
+    setYears(getAllYears());
+    console.log(years);
+  }, []);
 
   return (
-    <>
-      Years
+    <div className="main">
       <p>
-        <input type="radio" name="months-years" value="years" />
+        Years
+        <input
+          className="months-years"
+          type="radio"
+          name="months-years"
+          value="years"
+          onChange={changeSort}
+        />
       </p>
-      Months
       <p>
-        <input type="radio" name="months-years" value="months" />
+        Months
+        <input
+          className="months-years"
+          type="radio"
+          name="months-years"
+          value="months"
+          onChange={changeSort}
+        />
       </p>
       {returnMonths()}
       <div className="">{returnContainers()}</div>
-    </>
+    </div>
   );
 }
 
